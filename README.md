578bdb7b053d4cc9b1c39b35c4eb53b79bb76332
ParagGhatage <phghatage1@gmail.com>
2025-03-03 22:40:02 +0530

    changed summarization model to google/bigbird-pegasus-large-arxiv to summarize even longer article or research paper

--- README.md
+++ README.md
@@ -138,6 +138,7 @@ npm run dev
 
 ```bash
 cd backend
+pip install -r requirements.txt
 python -m app.main
 ```
 
--- backend/app/services/summarization_service.py
+++ backend/app/services/summarization_service.py
@@ -5,10 +5,11 @@ import os
 load_dotenv()
 hf_token = os.getenv("HF_TOKEN")
 
-Summarization_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+Summarization_URL = "https://api-inference.huggingface.co/models/google/bigbird-pegasus-large-arxiv"
 headers = {"Authorization": f"Bearer {hf_token}"}
 
 def summarize_text(payload):
+    
 	response = requests.post(Summarization_URL, headers=headers, json=payload)
 	return response.json()
     
